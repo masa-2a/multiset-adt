a97bf857b3c42a0e93247395ea2058ff08890319
masa <m.abuarja@mail.utoronto.ca>
2024-09-16 14:37:18 -0400

    completed tree class attributes and constructor

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,19 @@
+import java.util.List;
+
 public class Tree {
-    // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+
+private Integer root;
+private List<Tree> subtrees;
+
+public Tree(Integer root, List<Tree> subtrees) {
+    this.root = root;
+    if (subtrees == null) {
+        this.subtrees = null;
+    }
+    else {
+        this.subtrees = subtrees;
+    }
+
+}
+
 }
