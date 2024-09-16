a3d59c59f644efe4b89286d74f555f1947516e5a
Anh Dang Phuong <anh.dangphuong@mail.utoronto.ca>
2024-09-16 10:48:12 -0400

    updated TreeMultiSet

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,6 +1,11 @@
 public class TreeMultiSet extends MultiSet {
 
     // TODO add a Tree object as a private instance variable for this class.
+    private Tree tree;
+    public TreeMultiSet() {
+        tree = new Tree();
+    }
+
     /**
      * Add the given item to this multiset.
      *
@@ -9,6 +14,7 @@ public class TreeMultiSet extends MultiSet {
     @Override
     void add(Integer item) {
         // TODO complete this method
+        tree.insert(item);
     }
     // TODO write the rest of the implementation for this class so that it uses its private Tree
     //      object to provide the MultiSet functionality.
