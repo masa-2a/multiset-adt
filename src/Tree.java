15a0fad63b2a127ba6eb43dd62dc77d43cf37224
masa <m.abuarja@mail.utoronto.ca>
2024-09-16 15:09:34 -0400

    completed the following methods: is_empty, length and count.

--- src/Tree.java
+++ src/Tree.java
@@ -15,6 +15,44 @@ public class Tree {
         }
 
     }
-    //
+    //implemented is_empty
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    //impleted Length
+    public int length(){
+        if (this.isEmpty()) {
+            return 0;
+        }
+        else{
+            int size = 1;
+            for( Tree subtree : subtrees ) {
+                size += subtree.length();
+            }
+            return size;
+        }
+    }
+
+    //implemented count()
+    public int count(int number){
+        if (this.isEmpty()) {
+            return 0;
+        }
+        else{
+            int num = 0;
+            if(this.root == number){
+                num++;
+            }
+            for (Tree subtree : subtrees) {
+                num += subtree.count(number);
+            }
+            return num;
+        }
+    }
+
+    //TODO implement toString method using private str_indented method
+
+
 
 }
